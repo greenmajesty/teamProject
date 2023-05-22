226beb0832ea07962aa16faa91791c53abc3bd01
Wondonghui <pakston9@naver.com>
2023-05-22 17:32:29 +0900

    ver7

--- dist/detail.f874a451.js
+++ dist/detail.f874a451.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"css/sub.scss":[function(require,module,exports) {
+})({"css/detail.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
@@ -403,4 +403,4 @@ module.exports = function loadJSBundle(bundle) {
 },{}],0:[function(require,module,exports) {
 var b=require("node_modules/parcel-bundler/src/builtins/bundle-loader.js");b.register("js",require("node_modules/parcel-bundler/src/builtins/loaders/browser/js-loader.js"));b.load([]);
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js",0], null)
-//# sourceMappingURL=/sub.1cb441e7.js.map
+//# sourceMappingURL=/detail.f874a451.js.map
