6d7abde4ed2a62cae1c348d46900db502eb9734d
Wondonghui <pakston9@naver.com>
2023-05-26 14:03:48 +0900

    ver13

--- js/product_data.js
+++ js/product_data.js
@@ -134,7 +134,519 @@ const productList = [
       deliver: false,
       new: true
     },
-
+    {
+      id: "product16",
+      name: "LED 전구 Spherical E14 (5.7w)",
+      price: 5000,
+      src: "./img/1126.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product17",
+      name: "CFL 전구 Twin Tube 2-Pin (18w)",
+      price: 7500,
+      src: "./img/1127.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product18",
+      name: "할로겐 전구 Capsule G4 (20w)",
+      price: 3000,
+      src: "./img/1128.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product19",
+      name: "LED 전구 Classic A60 E27 (8.5w)",
+      price: 9000,
+      src: "./img/1129.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product20",
+      name: "CFL 전구 Triple Tube 4-Pin (26w)",
+      price: 8500,
+      src: "./img/1130.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product21",
+      name: "LED 전구 Classic B35 E14 (5.7w)",
+      price: 7000,
+      src: "./img/1131.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product22",
+      name: "산업용전구 MR16 GU5.3 (35w)",
+      price: 6500,
+      src: "./img/1132.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product23",
+      name: "LED 전구 F-LED TUBE R (14w, E26, Dimmable)",
+      price: 17000,
+      src: "./img/1133.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product24",
+      name: "LED 전구 Classic ST64 E27 (7.5w)",
+      price: 10000,
+      src: "./img/1134.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product25",
+      name: "할로겐 전구 Capsule GY6.35 (50w)",
+      price: 3500,
+      src: "./img/1135.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product26",
+      name: "LED 전구 Classic A70 E27 (10w)",
+      price: 11000,
+      src: "./img/1136.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product27",
+      name: "산업용전구 AR111 G53 (50w)",
+      price: 7000,
+      src: "./img/1137.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product28",
+      name: "CFL 전구 Twin Tube 4-Pin (13w)",
+      price: 6500,
+      src: "./img/1138.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product29",
+      name: "LED 전구 Spherical E27 (6w)",
+      price: 6000,
+      src: "./img/1139.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product30",
+      name: "할로겐 전구 Capsule G9 (40w)",
+      price: 3200,
+      src: "./img/1140.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product31",
+      name: "LED 전구 F-LED TUBE R (18w, E26, Dimmable)",
+      price: 18000,
+      src: "./img/1141.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product32",
+      name: "산업용전구 IR125 RED(400w, 하드글래스 열전구)",
+      price: 8000,
+      src: "./img/1142.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product33",
+      name: "CFL 전구 Triple Tube 2-Pin (20w)",
+      price: 7500,
+      src: "./img/1143.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product34",
+      name: "LED 전구 Classic B35 E14 (7w)",
+      price: 8000,
+      src: "./img/1144.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product35",
+      name: "할로겐 전구 Capsule GY6.35 (75w)",
+      price: 4000,
+      src: "./img/1145.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product36",
+      name: "LED 전구 Spherical E14 (7w)",
+      price: 5500,
+      src: "./img/1146.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product37",
+      name: "CFL 전구 Twin Tube 2-Pin (23w)",
+      price: 8000,
+      src: "./img/1147.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product38",
+      name: "할로겐 전구 Capsule G4 (30w)",
+      price: 3500,
+      src: "./img/1148.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product39",
+      name: "LED 전구 Classic A60 E27 (11w)",
+      price: 9500,
+      src: "./img/1149.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product40",
+      name: "CFL 전구 Triple Tube 4-Pin (32w)",
+      price: 9000,
+      src: "./img/1150.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product41",
+      name: "LED 전구 Classic B35 E14 (8.5w)",
+      price: 8500,
+      src: "./img/1151.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product42",
+      name: "산업용전구 MR16 GU5.3 (50w)",
+      price: 7000,
+      src: "./img/1152.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product43",
+      name: "LED 전구 F-LED TUBE R (21w, E26, Dimmable)",
+      price: 20000,
+      src: "./img/1153.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product44",
+      name: "LED 전구 Classic ST64 E27 (10w)",
+      price: 11500,
+      src: "./img/1154.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product45",
+      name: "할로겐 전구 Capsule GY6.35 (100w)",
+      price: 4500,
+      src: "./img/1155.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product46",
+      name: "LED 전구 Classic B35 E14 (9.5w)",
+      price: 9000,
+      src: "./img/1156.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product47",
+      name: "산업용전구 MR16 GU10 (75w)",
+      price: 8000,
+      src: "./img/1157.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product48",
+      name: "LED 전구 F-LED TUBE R (22w, E26, Dimmable)",
+      price: 21000,
+      src: "./img/1158.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product49",
+      name: "LED 전구 Classic ST64 E27 (12w)",
+      price: 12000,
+      src: "./img/1159.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product50",
+      name: "할로겐 전구 Capsule GY6.35 (125w)",
+      price: 5000,
+      src: "./img/1160.jpg",
+      best: false,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product51",
+      name: "LED 전구 Classic A60 E27 (12.5w)",
+      price: 10000,
+      src: "./img/1161.jpg",
+      best: true,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product52",
+      name: "CFL 전구 Quad Tube 4-Pin (42w)",
+      price: 10000,
+      src: "./img/1162.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product53",
+      name: "LED 전구 Classic B35 E14 (10.5w)",
+      price: 9000,
+      src: "./img/1163.jpg",
+      best: true,
+      deliver: false,
+      new: false
+    },
+    {
+      id: "product54",
+      name: "산업용전구 MR16 GU10 (100w)",
+      price: 9000,
+      src: "./img/1164.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product55",
+      name: "LED 전구 F-LED TUBE R (24w, E26, Dimmable)",
+      price: 22000,
+      src: "./img/1165.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product56",
+      name: "LED 전구 Classic ST64 E27 (14w)",
+      price: 13000,
+      src: "./img/1166.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product57",
+      name: "할로겐 전구 Capsule GY6.35 (150w)",
+      price: 5500,
+      src: "./img/1167.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product58",
+      name: "LED 전구 Classic A60 E27 (13.5w)",
+      price: 11000,
+      src: "./img/1168.jpg",
+      best: false,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product59",
+      name: "CFL 전구 Five Tube 4-Pin (52w)",
+      price: 11000,
+      src: "./img/1169.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product60",
+      name: "LED 전구 Classic B35 E14 (11.5w)",
+      price: 10000,
+      src: "./img/1170.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product61",
+      name: "산업용전구 MR16 GU10 (125w)",
+      price: 10000,
+      src: "./img/1171.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product62",
+      name: "LED 전구 F-LED TUBE R (26w, E26, Dimmable)",
+      price: 24000,
+      src: "./img/1172.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product63",
+      name: "LED 전구 Classic ST64 E27 (16w)",
+      price: 15000,
+      src: "./img/1173.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product64",
+      name: "할로겐 전구 Capsule GY6.35 (175w)",
+      price: 6000,
+      src: "./img/1174.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product65",
+      name: "LED 전구 Classic A60 E27 (15w)",
+      price: 12000,
+      src: "./img/1175.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    },
+    {
+      id: "product66",
+      name: "CFL 전구 Six Tube 4-Pin (60w)",
+      price: 12000,
+      src: "./img/1176.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product67",
+      name: "LED 전구 Classic B35 E14 (12.5w)",
+      price: 11000,
+      src: "./img/1177.jpg",
+      best: false,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product68",
+      name: "산업용전구 MR16 GU10 (150w)",
+      price: 11000,
+      src: "./img/1178.jpg",
+      best: true,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product69",
+      name: "LED 전구 F-LED TUBE R (28w, E26, Dimmable)",
+      price: 26000,
+      src: "./img/1179.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product70",
+      name: "LED 전구 Classic ST64 E27 (18w)",
+      price: 17000,
+      src: "./img/1180.jpg",
+      best: false,
+      deliver: false,
+      new: true
+    },
+    {
+      id: "product71",
+      name: "할로겐 전구 Capsule GY6.35 (200w)",
+      price: 6500,
+      src: "./img/1181.jpg",
+      best: true,
+      deliver: true,
+      new: false
+    },
+    {
+      id: "product72",
+      name: "LED 전구 Classic A60 E27 (16.5w)",
+      price: 13000,
+      src: "./img/1182.jpg",
+      best: false,
+      deliver: true,
+      new: true
+    }
   ];
   
   export default productList
--- js/sub.js
+++ js/sub.js
@@ -57,11 +57,21 @@ $(document).ready(function () {
         $(".footer_person_info").toggle();
     });
 
-    function createMainBoxes(productData) {
-        const $mainContainer = $(".main_container");
+    let currentPage = 1;
+    const itemsPerPage = 12;  // 한 페이지에 표시할 항목의 수
     
-        for (let i = 0; i < 12 && i < productData.length; i++) {
-            const product = productData[i];
+    function createMainBoxes(productData, page) {
+        const $mainContainer = $(".main_container");
+        $mainContainer.empty();  // 현재 페이지의 내용을 지웁니다.
+    
+        const start = (page - 1) * itemsPerPage;  // 시작 인덱스
+        const end = start + itemsPerPage;  // 끝 인덱스
+    
+        // 시작과 끝 인덱스 사이의 데이터만 가져옵니다.
+        const pageItems = productData.slice(start, end);
+        
+        for (let i = 0; i < pageItems.length; i++) {
+            const product = pageItems[i];
             let $mainBox = $(`<a href="detail.html?id=${product.id}" class="main_box"></a>`);
     
             if(product.best){
@@ -83,5 +93,44 @@ $(document).ready(function () {
         }
     }
     
-    createMainBoxes(productList);
+
+// 가장 첫 페이지와 마지막 페이지 번호를 변수에 할당합니다.
+const firstPage = 1;
+const lastPage = Math.ceil(productList.length / itemsPerPage);
+
+// 페이지 버튼 클릭 이벤트
+$(".pagenation > div").click(function() {
+    const $this = $(this);
+    const buttonText = $this.text();
+
+    $(".pagenation > div").removeClass('now_page');
+
+    switch (buttonText) {
+        case "<<":
+            currentPage = firstPage;
+            break;
+        case "<":
+            if (currentPage > firstPage) { // 현재 페이지가 첫 페이지보다 크면
+                currentPage--;
+            }
+            break;
+        case ">":
+            if (currentPage < lastPage) { // 현재 페이지가 마지막 페이지보다 작으면
+                currentPage++;
+            }
+            break;
+        case ">>":
+            currentPage = lastPage;
+            break;
+        default:
+            const pageNo = parseInt(buttonText, 10);
+            if (!isNaN(pageNo)) {
+                currentPage = pageNo;
+            }
+    }
+    $(".pagenation > div").eq(currentPage + 1).addClass('now_page');
+    createMainBoxes(productList, currentPage);
+});
+    
+    createMainBoxes(productList, currentPage);
 });
